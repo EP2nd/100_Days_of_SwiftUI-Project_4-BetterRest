ff8879e70e6e2cd62bb6b6be755f80e2150b7b1c
EPJr <edwin.przezwiecki.jr@icloud.com>
2022-11-28 20:28:12 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Edwin Przeźwiecki Jr. on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
